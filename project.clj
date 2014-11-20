a04bfb9ae7294a16e75795dc0e799a5a671eb0d7
Shriphani Palakodety <shriphanip@gmail.com>
2014-11-20 04:32:45 -0500

    can begin working on the iteration section

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[net.mikera/vectorz-clj "0.26.2"]
+  :dependencies [[com.github.fommil.netlib/all "1.1.2" :extension pom]
+                 [net.mikera/vectorz-clj "0.26.2"]
                  [net.mikera/core.matrix "0.31.1"]
                  [org.clojure/clojure "1.6.0"]])
--- src/thin_svd/core.clj
+++ src/thin_svd/core.clj
@@ -1,7 +1,9 @@
 (ns thin-svd.core
   (:refer-clojure :exclude [* - + == /])
   (:require [clojure.core.matrix :refer :all]
-            [clojure.core.matrix.operators :refer :all]))
+            [clojure.core.matrix.operators :refer :all])
+  (:import [com.github.fommil.netlib ARPACK]
+           [org.netlib.util intW doubleW]))
 
 (defn eigen-decomposition
   "Args:
@@ -11,4 +13,51 @@
     tol: tolerance
     max-iterations: number of iterations to stop after"
   [mul dimension num-evs tol max-iterations]
-  '*)
+
+  (let [arpack (ARPACK/getInstance)
+
+        tolW   (doubleW. tol)
+        nev    (intW. num-evs)
+
+        ncv    (min (* 2 num-evs)
+                    dimension)
+
+        bmat   "I"
+        which  "LM"
+
+        iparam (int-array 11)
+
+        _ (aset-int iparam 0 1)
+        _ (aset-int iparam 2 max-iterations)
+        _ (aset-int iparam 6 1)
+
+        ido (intW. 0)
+        info (intW. 0)
+        resid (double-array dimension)
+        v     (double-array (* dimension
+                               ncv))
+        workd (double-array (* ncv
+                               3))
+        workl (double-array (* ncv
+                               (+ 8 ncv)))
+        ipntr (int-array 11)]
+    
+    (.dsaupd arpack
+             ido
+             bmat
+             dimension
+             which
+             (.val nev)
+             tolW
+             resid
+             ncv
+             v
+             dimension
+             iparam
+             ipntr
+             workd
+             workl
+             (count workl)
+             info)
+    (let [w (vector workd)]
+      )))
