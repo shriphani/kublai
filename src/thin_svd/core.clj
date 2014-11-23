4997b3fd71ca78ff42b452bc2485ba1c2e27bd5f
Shriphani Palakodety <shriphanip@gmail.com>
2014-11-22 19:47:52 -0500

    fixed bugs in symmetric eigenvalue problem

--- src/thin_svd/core.clj
+++ src/thin_svd/core.clj
@@ -13,129 +13,172 @@
     k: number of eigenvalues
     tol: tolerance
     max-iterations: number of iterations to stop after"
-  [mul dimension num-evs tol max-iterations]
+  ([mul dimension num-evs]
+     (eigen-decomposition mul dimension num-evs 1e-10 8))
 
-  (let [arpack (ARPACK/getInstance)
+  ([mul dimension num-evs tol max-iterations]
 
-        tolW   (doubleW. tol)
-        nev    (intW. num-evs)
+     (let [arpack (ARPACK/getInstance)
 
-        ncv    (min (* 2 num-evs)
-                    dimension)
+           tolW   (doubleW. tol)
+           nev    (intW. num-evs)
 
-        bmat   "I"
-        which  "LM"
+           ncv    (min (* 2 num-evs)
+                       dimension)
 
-        iparam (int-array 11)
+           bmat   "I"
+           which  "LM"
 
-        _ (aset-int iparam 0 1)
-        _ (aset-int iparam 2 max-iterations)
-        _ (aset-int iparam 6 1)
+           iparam (int-array 11)
 
-        ido (intW. 0)
-        info (intW. 0)
-        resid (double-array dimension)
-        v     (double-array (* dimension
-                               ncv))
-        workd (double-array (* ncv
-                               3))
-        workl (double-array (* ncv
-                               (+ 8 ncv)))
-        ipntr (int-array 11)]
-    
-    (.dsaupd arpack
-             ido
-             bmat
-             dimension
-             which
-             (.val nev)
-             tolW
-             resid
-             ncv
-             v
-             dimension
-             iparam
-             ipntr
-             workd
-             workl
-             (count workl)
-             info)
-    (let [w (array
-             (vec workd))]
+           _ (aset-int iparam 0 1)
+           _ (aset-int iparam 2 max-iterations)
+           _ (aset-int iparam 6 1)
 
-      (while (not= 99 (.val ido))      
-        (if-not (and (not= 1 (.val ido))
-                     (not= -1 (.val ido)))
-          (let [input-offset (- (aget ipntr 0) 1)
-                output-offset (- (aget ipntr 1) 1)
+           ido (intW. 0)
+           info (intW. 0)
+           resid (double-array dimension)
+           v     (double-array (* dimension
+                                  ncv))
+           workd (double-array (* ncv
+                                  3))
+           workl (double-array (* ncv
+                                  (+ 8 ncv)))
+           ipntr (int-array 11)]
+       
+       (.dsaupd arpack
+                ido
+                bmat
+                dimension
+                which
+                (.val nev)
+                tolW
+                resid
+                ncv
+                v
+                dimension
+                iparam
+                ipntr
+                workd
+                workl
+                (count workl)
+                info)
 
-                x (subvector w
-                             input-offset
-                             (min dimension
-                                  (- (-> w shape first)
-                                     input-offset)))
-                y (subvector w
-                             output-offset
-                             (min dimension
-                                  (- (-> w shape first)
-                                     output-offset)))
-                
-                new-y (mul x)]
-            (.dsaupd arpack
-                     ido
-                     bmat
-                     dimension
-                     which
-                     (.val nev)
-                     tolW
-                     resid
-                     ncv
-                     v
-                     dimension
-                     iparam
-                     ipntr
-                     workd
-                     workl
-                     (count workl)
-                     info))))
+       (while (not= 99 (.val ido))      
+         (if-not (and (not= 1 (.val ido))
+                      (not= -1 (.val ido)))
+           (let [w (array
+                    (vec workd))
+                 input-offset (- (aget ipntr 0) 1)
+                 output-offset (- (aget ipntr 1) 1)
 
-      (let [d (double-array (.val nev))
-            select (boolean-array ncv)
-            z (Arrays/copyOfRange v 0 (* (.val nev) dimension))]
-        (.dseupd arpack
-                 true
-                 "A"
-                 select
-                 d
-                 z
-                 dimension
-                 0.0
-                 bmat
-                 dimension
-                 which
-                 nev
-                 tol
-                 resid
-                 ncv
-                 v
-                 dimension
-                 iparam
-                 ipntr
-                 workd
-                 workl
-                 (count workl)
-                 info)
+                 x (subvector w
+                              input-offset
+                              (min dimension
+                                   (- (-> w shape first)
+                                      input-offset)))
+                 y (subvector w
+                              output-offset
+                              (min dimension
+                                   (- (-> w shape first)
+                                      output-offset)))
+                 
+                 new-y (mul x)
+                 _ (println x)
+                 _ (println workd)
+                 _ (println new-y)
+                 new-workd (reduce
+                            (fn [acc i]
+                              (println acc)
+                              (aset-double acc
+                                           i
+                                           (mget new-y
+                                                 (- i output-offset)))
+                              acc)
+                            workd
+                            (range output-offset
+                                   (+ output-offset
+                                      (-> new-y shape first))))]
+             (.dsaupd arpack
+                      ido
+                      bmat
+                      dimension
+                      which
+                      (.val nev)
+                      tolW
+                      resid
+                      ncv
+                      v
+                      dimension
+                      iparam
+                      ipntr
+                      new-workd
+                      workl
+                      (count workl)
+                      info))
+           (throw
+            (Exception. "Fatal error encountered"))))
 
-        (let [computed (aget iparam 4)
-              eigenpairs (sort-by
-                          first
-                          (map-indexed
-                           (fn [i x]
-                             [x
-                              (vec
-                               (Arrays/copyOfRange z (* i dimension) (+ dimension (* i dimension))))])
-                           (Arrays/copyOfRange d 0 computed)))
-              eigenvector-matrix (matrix (map second eigenpairs))
-              eigenvalue-matrix  (diagonal-matrix (map first eigenpairs))]
-          {:Q eigenvector-matrix
-           :A eigenvalue-matrix})))))
+       (let [d (double-array (.val nev))
+             select (boolean-array ncv)
+             z (Arrays/copyOfRange v 0 (* (.val nev) dimension))]
+         (.dseupd arpack
+                  true
+                  "A"
+                  select
+                  d
+                  z
+                  dimension
+                  0.0
+                  bmat
+                  dimension
+                  which
+                  nev
+                  tol
+                  resid
+                  ncv
+                  v
+                  dimension
+                  iparam
+                  ipntr
+                  workd
+                  workl
+                  (count workl)
+                  info)
+
+         (let [computed (aget iparam 4)
+               eigenpairs (reverse
+                           (sort-by
+                            first
+                            (map-indexed
+                             (fn [i x]
+                               [x
+                                (vec
+                                 (Arrays/copyOfRange z (* i dimension) (+ dimension (* i dimension))))])
+                             (Arrays/copyOfRange d 0 computed))))
+               eigenvector-matrix (matrix (map second eigenpairs))
+               eigenvalue-matrix  (diagonal-matrix (map first eigenpairs))]
+           {:Q eigenvector-matrix
+            :A eigenvalue-matrix})))))
+
+(defn svd
+  "Args: we expect a matrix and the number
+   of singular values you want to retrieve"
+  [M n]
+  (let [gram-matrix (* (transpose M)
+                       M)
+
+        cov-matrix  (* M (transpose M))
+
+        gram-dimension (-> gram-matrix shape first)
+        cov-dimension  (-> cov-matrix shape first)
+        
+        {U :Q lambda :A} (eigen-decomposition #(* cov-matrix %)
+                                         cov-dimension
+                                         n)
+        {V :Q lambda2 :A} (eigen-decomposition #(* gram-matrix %)
+                                         gram-dimension
+                                         n)]
+    {:U U
+     :V* (transpose V)
+     :S (sqrt lambda)}))
--- src/thin_svd/test_setup.clj
+++ src/thin_svd/test_setup.clj
@@ -1,6 +1,7 @@
 (ns thin-svd.test-setup
   (:refer-clojure :exclude [* - + == /])
   (:require [clojure.core.matrix :refer :all]
+            [clojure.core.matrix.linear :refer :all]
             [clojure.core.matrix.operators :refer :all]
             [thin-svd.core :as core]))
 
@@ -10,13 +11,13 @@
   "Build a matrix and svd it"
   []
   (let [M (matrix [[1 2 3 4]
-                   [5 6 7 8]
-                   [9 10 11 12]
-                   [13 14 15 16]])
-        mul #(* M %)
+                   [2 5 6 7]
+                   [3 6 8 9]
+                   [4 7 9 10]])
+        mul #(mmul M %)
         dimension 4
         num-evs 2
-        tol 1e-6
-        max-iterations 100]
+        tol 0.0
+        max-iterations 300]
     (core/eigen-decomposition mul dimension num-evs tol max-iterations)))
 
