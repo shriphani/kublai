cca2a91ade447eec83e5f1b403a9bf4ffc5d4a63
Shriphani Palakodety <shriphanip@gmail.com>
2014-11-28 04:51:06 -0500

    snapshot to commit

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject kublai "0.1.0-SNAPSHOT"
+(defproject kublai "0.9-SNAPSHOT"
   :description "Truncated decompositions for core.matrix"
   :url "http://github.com/shriphani/kublai"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.github.fommil.netlib/all "1.1.2" :extension pom]
-                 [net.mikera/vectorz-clj "0.26.2"]
                  [net.mikera/core.matrix "0.31.1"]
                  [org.clojure/clojure "1.6.0"]])
